751c2a1774b44d40f2e4dc7c65c3cc8a4a54b168
boriratkk-boop <boriratkk@gmail.com>
2026-01-02 22:28:56 +0700

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -3,10 +3,11 @@ const { defineConfig } = require('@playwright/test');
 module.exports = defineConfig({
   testDir: './tests',
   timeout: 30000,
+  reporter: [['html'], ['list']],
   use: {
-    headless: true,               // รัน CI แบบไม่เห็น browser
-    screenshot: 'only-on-failure',
-    video: 'retain-on-failure'
-  },
-  reporter: [['html'], ['list']]
+    headless: true,
+    screenshot: 'on',          // 🔥 แคปทุก step
+    video: 'on',               // 🔥 อัด video
+    trace: 'off'
+  }
 });
