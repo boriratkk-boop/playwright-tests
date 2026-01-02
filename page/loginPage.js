3f9779de5ad2392f0fec13fdc50d4527d0ecfde4
boriratkk-boop <boriratkk@gmail.com>
2026-01-02 21:50:17 +0700

    Update loginPage.js

--- page/loginPage.js
+++ page/loginPage.js
@@ -3,7 +3,7 @@ class LoginPage {
     this.page = page;
     this.usernameInput = page.locator('#user-name');
     this.passwordInput = page.locator('#password');
-    this.loginBtn = page.locator('#login-button');
+    this.loginButton = page.locator('#login-button');
   }
 
   async goto() {
@@ -13,7 +13,7 @@ class LoginPage {
   async login(username, password) {
     await this.usernameInput.fill(username);
     await this.passwordInput.fill(password);
-    await this.loginBtn.click();
+    await this.loginButton.click();
   }
 }
 
