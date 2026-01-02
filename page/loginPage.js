8f69d4a416548cc1dc68ca2a29d7cd639212599b
boriratkk-boop <boriratkk@gmail.com>
2026-01-02 22:05:51 +0700

    Update loginPage.js

--- page/loginPage.js
+++ page/loginPage.js
@@ -8,12 +8,14 @@ class LoginPage {
 
   async goto() {
     await this.page.goto('https://www.saucedemo.com/');
+    await this.page.screenshot({ path: 'screenshots/01-login-page.png' });
   }
 
   async login(username, password) {
     await this.usernameInput.fill(username);
     await this.passwordInput.fill(password);
     await this.loginButton.click();
+    await this.page.screenshot({ path: 'screenshots/02-after-login.png' });
   }
 }
 
